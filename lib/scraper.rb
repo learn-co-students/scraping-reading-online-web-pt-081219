d9efca64358c91f09c32ec4a5f7adc14e178a5ea
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-09-26 20:59:57 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
+require "pry"
 
-html = open("https://flatironschool.com/")
+
+ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+ 
+ Nokogiri::HTML(html)
+ doc.css(".headline-26OIBN").text  
